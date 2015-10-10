dee612b8b6421daacef3109825e1c54bfc4381b0
lujjjh <i@codehut.me>
2015-10-10 22:30:25 +0800

    Initial Commit

---
+++ simple-http-server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple-http-server
+//
+//  Created by lujjjh on 15/10/10.
+//  Copyright © 2015年 ljh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
